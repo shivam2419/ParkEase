bff1bb41485802bb0af3e98301ab132cfa37ee66
Shivam Sharma <120017051+shivam2419@users.noreply.github.com>
2024-01-21 20:12:51 +0530

    Add files via upload

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Register)
+admin.site.register(carRegister)
 admin.site.register(signUp)
+admin.site.register(testid)
---
+++ myApp/keys_twilio.py
@@ -0,0 +1,4 @@
+account_sid = "AC0b1623e65a736f0370689ad09cdfa004"
+auth_token= "5c124db854bc6d26cc7190d07b5bb766"
+twilio_number = "+15415260353"
+my_phone_number = "+919999514480"
